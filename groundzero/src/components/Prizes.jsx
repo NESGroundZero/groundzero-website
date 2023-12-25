a2a0ad7b5b29eeab088ca638d212405ea8911399
wjacobw <williamjacob0910@gmail.com>
2023-12-25 23:47:56 +0800

    make sizedbox taller

--- groundzero/src/components/Prizes.jsx
+++ groundzero/src/components/Prizes.jsx
@@ -1,13 +1,65 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
+import styles from "../style";
 
-function Prizes() {
+const PrizeBox = ({ place, amount, height }) => {
+  // You might want to ensure that the height value is set correctly, for example, `h-32`
+  // or use inline styles if needed: style={{ height }}
   return (
-    <div>
-      <NavigationBar />
-      Prizes
+    <div
+      className={`relative text-center p-4 w-64 ${height}`}
+      style={{
+        background:
+          "linear-gradient(to right, rgba(32,27,71,255), rgba(19,33,85,255), rgba(0,42,98,255))",
+        boxShadow:
+          "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      }}
+    >
+      <div className="absolute inset-x-0 top-0 transform -translate-y-1/2">
+        <div
+          className="mx-auto h-16 w-36  bg-blue-200 rounded-3xl text-sm font-light flex items-center justify-center shadow-lg"
+          style={{
+            background:
+              "linear-gradient(to right, rgba(92,136,250,255), rgba(67,73,248,255), rgba(97,76,244,255))",
+            boxShadow:
+              "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+          }}
+        >
+          <span className="text-white">{place}</span>
+        </div>
+      </div>
+      <div className="mt-6">
+        <span className="text-white text-2xl font-light">{amount}</span>
+      </div>
     </div>
   );
-}
+};
 
-export default Prizes;
+const PrizeComponent = () => {
+  return (
+    <div className="bg-purple w-full overflow-hidden">
+      <NavigationBar />
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <div className={`${styles.paddingY} flex-col relative`} id="about">
+            <div className="grid place-items-center text-white xs:px-16 lg:px-40">
+              <div className="text-center text-white">
+                <div className="heading font-light">PRIZES</div>
+                <p className="text-4xl my-14">worth $10,000</p>
+                <div className="flex justify-center items-end mt-14">
+                  <PrizeBox place="2nd" amount="$3,000" height="h-32" />
+                  <div className="relative">
+                    <PrizeBox place="1st" amount="$4,000" height="h-40" />
+                  </div>
+                  <PrizeBox place="3rd" amount="$2,000" height="h-32" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrizeComponent;
--- groundzero/src/components/ReasonsToJoin.jsx
+++ groundzero/src/components/ReasonsToJoin.jsx
@@ -1,34 +1,47 @@
-import React from 'react'
+import React from "react";
 import styles from "../style";
 import StatementCard from "./problem-statement-components/StatementCard";
 
 const ReasonsToJoin = () => {
   return (
     <div className="reasons-to-join">
-      <div className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      <div
+        className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+      >
         <div className="w-full text-center justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-          <h1 className={styles.heading2}>Why Should You Join Ground Zero?</h1>
+          <h1 className={styles.heading2}>Why Ground Zero</h1>
         </div>
-        <div className={`${styles.flexCenter} flex-wrap w-full`}>
-          <div className="grid gap-x-6 gap-y-10 lg:grid-cols-3 md:w-[80%] w-[70%]">
-            {generateCards()}
+        <div className="grid place-items-center text-white xs:px-16 lg:px-40">
+          <div className="pb-10"></div>
+
+          <div className="font-light md:text-2xl text-[16px] pb-9 text-justify">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu
+              lacinia ex. Mauris dapibus magna ut dui porttitor, nec mollis
+              metus iaculis. Nam tortor nibh, placerat quis leo non, suscipit
+              scelerisque purus. Quisque porttitor viverra lorem eu porttitor.
+              Duis lacinia magna non bibendum venenatis. Integer sagittis nisl
+              ante, at tincidunt sapien faucibus ut. Praesent eleifend lectus
+              nec elit congue, sed commodo justo molestie. Pellentesque
+              fermentum dolor ut lectus cursus, imperdiet luctus ipsum volutpat.
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const generateCards = () => {
   let content = [];
-  for(let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     content.push(
       <div>
-        <StatementCard id={i} type="reason"/>
+        <StatementCard id={i} type="reason" />
       </div>
     );
   }
   return content;
-}
+};
 
-export default ReasonsToJoin
+export default ReasonsToJoin;
