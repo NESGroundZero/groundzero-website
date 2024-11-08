619861a4ab3d741124e27db6113fa37bc15fabc9
emo-boy-uwu <khoatlong29@gmail.com>
2024-11-09 03:00:13 +0800

    Fix responsive issues

--- groundzero/src/pages/PartnersBeforeEvent.jsx
+++ groundzero/src/pages/PartnersBeforeEvent.jsx
@@ -16,8 +16,8 @@ function PartnersBeforeEvent() {
           <PastEvents/>
           <div className="sponsors-container">
             <div className="sponsor-level-others">Past Sponsors</div>
-            <div className='sponsor-logo-container-s bg-white'>
-              <img src={pastSponsors} className="mx-auto mt-10 w-full bg-black max-w-[300px] md:max-w-[600px] lg:max-w-[700px] xl:max-w-[800px] h-auto" alt="Sponsor Logo" />
+            <div className='sponsor-logo-container-s'>
+              <img src={pastSponsors} className="mx-auto mt-10 w-full max-w-[300px] md:max-w-[600px] lg:max-w-[700px] xl:max-w-[800px] h-auto" alt="Sponsor Logo" />
             </div>
           </div>
       </div>
